b6d7d37bfd765c8b0e82bad057f7efc9f50435c4
WillisLi <li.willis95@gmail.com>
2021-08-29 19:05:14 -0700

    created React App and set up some components

---
+++ genshinsphere/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className = "genshinSphereApp">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          test
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ genshinsphere/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// const store = createStore();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+reportWebVitals();
