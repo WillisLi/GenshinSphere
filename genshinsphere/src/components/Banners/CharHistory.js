70e10ce63e0a3c691a3df0de9d9ca9f57fe49f91
WillisLi <li.willis95@gmail.com>
2021-10-23 23:18:07 -0700

    generalized table component for multi-purpose usage and refactored charhistory component

--- genshinsphere/src/components/Banners/CharHistory.js
+++ genshinsphere/src/components/Banners/CharHistory.js
@@ -1,27 +1,16 @@
 import React from 'react'
 import axios from 'axios';
-import { useQuery, useQueries } from 'react-query';
-import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import logo from '../../logo.svg'
 import './CharHistory.css'
+import EntityTable from './EntityTable';
 
 const fetchCharacterBannerHistory = async () => {
     const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/banners/character`)
     return data;
 }
-
-const fetchCharacterIcon = async (characterName) => {
-    const { config } = await axios.get(`${process.env.REACT_APP_API_URL}/characters/${characterName}/icon`)
-    return config;
-}
-
 const CharHistory = () => {
     const { data: characters, status: charStatus, isLoading: charLoading, error: charError } = useQuery('characters', fetchCharacterBannerHistory)
-
-    // const { data: characterIcon, status: iconStatus} = useQuery(['characterIcon', characterName], () => fetchCharacterIcon(characterName), {
-    //     staleTime: 200000,
-    //     enabled: !!
-    // })
 
     const propList = (data, type) => {
         let list = []
@@ -38,50 +27,11 @@ const CharHistory = () => {
         return list
     }
 
-    const countChar = (data, character) => {
-        const countHistory = []
-        let count = 0;
-        for (let idx = 0; idx < data.length; idx++)  {
-            const { featured } = data[idx]
-            if (!countHistory.includes("featured") && !featured.includes(character)) {
-                countHistory.push(null);
-            } else if (countHistory.includes("featured") && !featured.includes(character)) {
-                count += 1;
-                countHistory.push(count)
-            } else if (featured.includes(character)) {
-                count = 0;
-                countHistory.push('featured');
-            }
-        }
-        return countHistory;
-    }
-
     return (
         <div className = "archivePage">
-            <div className = "tableContainer">
-                <table className = "historyTable">
-                    <thead>
-                        <tr>
-                            <th>Character</th>
-                            {charStatus === 'success' && propList(characters, 'version').sort().map(versionNum => (
-                            <th>
-                                <td>{versionNum}</td>
-                            </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {charStatus === 'success' && propList(characters, 'featured').sort().map(charName => (
-                            <tr>
-                                <td>{charName}</td>
-                                {charStatus === 'success' && countChar(characters, `${charName}`).map(history => (
-                                <td>{history}</td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+            {charStatus === "success" &&
+            <EntityTable entityNames = {propList(characters, "featured")} history = {characters} versions = {propList(characters, "version")} type = "characters"/>
+            }
         </div>
     );
 }
--- genshinsphere/src/components/Banners/EntityTable.js
+++ genshinsphere/src/components/Banners/EntityTable.js
@@ -1,17 +1,32 @@
 import React from "react";
 import axios from 'axios';
-import { useQuery } from 'react-query';
+import { useQueries, useIsFetching } from 'react-query';
+import { useEffect, useState } from "react";
 import './EntityTable.css'
 
 const fetchIcon = async (name, type) => {
-    const { config } = await axios.get(`${process.env.REACT_APP_API_URL}/characters/${name}/icon`)
-    return config;
+    const { config } = await axios.get(`${process.env.REACT_APP_API_URL}/${type}/${name}/icon`)
+    return config.url;
 }
 
-const EntityTable = ({dataSet, type}) => {
-    const { data: icon, status: iconStatus} = useQuery(['icon', dataSet], () => fetchIcon(dataSet, type), {
-        staleTime: 200000,
-    })
+const EntityTable = ({entityNames, history, versions, type}) => {
+    const results = useQueries(entityNames.map(entityName => ({
+            queryKey: ["icon", entityName],
+            queryFn: () => fetchIcon(entityName.toLowerCase(), type),
+    })))
+    const isFetching = useIsFetching();
+
+    if (isFetching === 0 && results[0].status === "success") {
+        console.log('yay')
+        console.log(results)
+    }
+    const getIcon = (resultsData, characterName) => {
+        for (let idx = 0; idx < resultsData.length; idx++) {
+            if (resultsData[idx].data.includes(characterName)) {
+                return resultsData[idx].data
+            }
+        }
+    }
 
     const countChar = (data, character) => {
         const countHistory = []
@@ -33,29 +48,29 @@ const EntityTable = ({dataSet, type}) => {
     
     return (
         <div className = "tableContainer">
-                <table className = "historyTable">
-                    <thead>
+            <table className = "historyTable">
+                <thead>
+                    <tr>
+                        <th>Character</th>
+                        {isFetching === 0 && results[0].status === 'success' && versions.map(versionNum => (
+                        <th>
+                            <td>{versionNum}</td>
+                        </th>
+                        ))}
+                        </tr>
+                </thead>
+                <tbody>
+                    {isFetching === 0 && results[0].status === 'success' && entityNames.sort().map(charName => (
                         <tr>
-                            <th>Character</th>
-                            {charStatus === 'success' && propList(characters, 'version').sort().map(versionNum => (
-                            <th>
-                                <td>{versionNum}</td>
-                            </th>
+                            <td>{charName}</td>
+                            {isFetching === 0 && results[0].status === 'success' && countChar(history, `${charName}`).map(entityHistory => (
+                                entityHistory === 'featured' ? <td><img src = {getIcon(results, charName.toLowerCase())} alt = {`${charName}`}/></td> : <td>{entityHistory}</td>
                             ))}
                         </tr>
-                    </thead>
-                    <tbody>
-                        {charStatus === 'success' && propList(characters, 'featured').sort().map(charName => (
-                            <tr>
-                                <td>{charName}</td>
-                                {charStatus === 'success' && countChar(characters, `${charName}`).map(history => (
-                                <td>{history}</td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
