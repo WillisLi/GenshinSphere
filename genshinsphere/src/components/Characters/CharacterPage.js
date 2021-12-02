b0226c634757bc335da5419b712d18ded20cad59
WillisLi <li.willis95@gmail.com>
2021-12-01 23:42:47 -0800

    added talents information to character page

--- genshinsphere/src/components/Characters/CharacterPage.js
+++ genshinsphere/src/components/Characters/CharacterPage.js
@@ -63,7 +63,36 @@ const CharacterPage = () => {
                 </div>
             </div>}
             <div className = "characterBody">
-
+                <div className = "combatTalents">
+                    <h2>Combat Talents</h2>
+                    {status === "success" && character.combatTalents.map((skill, index) => (
+                        <div key = {index} className = "skill">
+                            <h3>{skill.type + ": " + skill.title}</h3>
+                            {skill.excerpt && <p style = {{margin: "2% 1%"}}>{skill.excerpt}</p>}
+                            <p style={{whiteSpace: "pre-line"}}>{skill.description}</p>
+                            <table>
+                                <tbody>
+                                {skill.info.map((damage, index) => (
+                                    <tr key = {index}>
+                                        <th>{damage.type}</th>
+                                        <td>{damage.value}</td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    ))}
+                </div>
+                <div className = "passiveTalents">
+                    <h2>Passive Talents</h2>
+                    {status === "success" && character.passiveTalents.map((skill, index) => (
+                        <div key = {index} className = "skill">
+                            <h3>{skill.title}</h3>
+                            <p style = {{margin: "2% 1%"}}>Requirement: {skill.requirement}</p>
+                            <p style={{whiteSpace: "pre-line"}}>{skill.description}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
